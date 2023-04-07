181ec91c92bf2adec52a8496dec976e80cbc1c2c
Julian Martinez <julianmsbar@gmail.com>
2023-04-07 16:26:38 -0300

    Primera pre entrega

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,9 +1,14 @@
 
 
-let lugarDelafila =parseInt(prompt("¿Cual es tu lugar en la fila?"));
 
-for (let lugar = 1; lugar <= lugarDelafila; lugar++) {
-    let nombre = prompt("¿Como es tu nombre?");
-    console.log("Tu lugar " + nombre + ", es el numero" + lugar);
+
+
+
+let valor1 = prompt("¿Cual es el equipo mas grande de Uruguay?")
+
+while (valor1.toUpperCase() != "peñarol") {
+    
+    alert("Me gusta ese quipo, arriba " + valor1 + "!!")
+
+    valor1 = prompt ("¿Te gusta otro equipo?")
 }
-
---
+++ scripts/trash.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+let lugarDelafila = parseInt(prompt("¿Cual es tu lugar en la fila?"));
+for (let lugar = 1; lugar <= lugarDelafila; lugar++) {
+    let nombre = prompt("¿Como es tu nombre?");
+    console.log("Tu lugar " + nombre + ", es el numero " + lugar);
+}
+
+// Ejemplo clase
+
+
+let valor1 =parseInt(
+    prompt("¿Hasta que numero imprimiremos?")
+);
+for (let x = 1; x < 200; x++) {
+    if (valor1 + 1 == x) {
+        break;
+    }
+    console.log(x);
+
+    while valor1
+}
+
+
+// otro ejemplo
