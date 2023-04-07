f8e36ac32e202147435362bdfecb6a9344c6e6b5
Julian Martinez <julianmsbar@gmail.com>
2023-04-07 12:57:29 -0300

    Primera pre entrega proyecto, ciclo e iteraciones

---
+++ scripts/myscripts.js
@@ -0,0 +1,9 @@
+
+
+let lugarDelafila =parseInt(prompt("¿Cual es tu lugar en la fila?"));
+
+for (let lugar = 1; lugar <= lugarDelafila; lugar++) {
+    let nombre = prompt("¿Como es tu nombre?");
+    console.log("Tu lugar " + nombre + ", es el numero" + lugar);
+}
+
