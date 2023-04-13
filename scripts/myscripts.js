8777d5ed7e95f824e716b31c91c73fff6781061d
Julian Martinez <julianmsbar@gmail.com>
2023-04-13 13:19:20 -0300

    Fix primera entrega

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,14 +1,55 @@
 
 
 
+function solicitarServicio() {
 
+    let servicioSolicitado = prompt("Buen dia, ¿Que tipo de servicio estas buscando; vender, comprar o alquilar?")
 
+        if (servicioSolicitado == "comprar") {
+        alert ("Continúa por nuestro catalogo.")
+        }
+        else if (servicioSolicitado == "vender") {
+        var cantidadPosibleVenta = parseInt(prompt("¿Cuántas prendas nos quieres vender?"))
+        }
+        else if (servicioSolicitado == "alquilar") {
+        var posibleAlquiler = parseInt(prompt ("Coloca aqui el codigo de la prenda que quieres alquilar"))
+    }
 
-let valor1 = prompt("¿Cual es el equipo mas grande de Uruguay?")
+    console.log(servicioSolicitado)
+    console.log(cantidadPosibleVenta)
+    console.log(posibleAlquiler)
 
-while (valor1.toUpperCase() != "peñarol") {
+    for(let i = 1; i <= cantidadPosibleVenta; i++){
+        console.log(i);
+    }
     
-    alert("Me gusta ese quipo, arriba " + valor1 + "!!")
-
-    valor1 = prompt ("¿Te gusta otro equipo?")
 }
+
+solicitarServicio()
+
+
+
+
+
+
+
+
+
+
+
+//var valorPosibleVenta = 0 ;
+
+//while (valorPosibleVenta => 100 ) {
+
+    //console.log("Ha ingresado una venta carisima a nuestra tienda")
+
+   // valorPosibleVenta++;
+//}
+
+
+
+
+
+//for (var t = )while (valor1.toUpperCase() != "peñarol") {
+    
+  //  alert("Me gusta ese quipo, arriba " + valor1 + "!!")
